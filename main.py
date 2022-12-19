cb0c6a12eb1adb65804f8fb54eb27d7d60b888e5
George <GeorgeGreen.QA@yandex.ru>
2022-12-19 19:48:03 +0400

    added testing map

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame, sys
+from settings import *
+from level import Level
+
 
 # Pygame setup
-
 pygame.init()
-screen_width = 1200
-screen_height = 700
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
 
 while True:
     for event in pygame.event.get():
@@ -14,7 +15,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-        screen.fill('red')
+        screen.fill('black')
+        level.run()
 
         pygame.display.update()
         clock.tick(60)
---
+++ settings.py
@@ -0,0 +1,17 @@
+level_map = [
+'                                        ',
+'                                        ',
+'                                        ',
+'                                        ',
+'                                        ',
+'  XX      XXX                    XX     ',
+'XX                                      ',
+'XXXXX            XXXX          XXXX     ',
+'XXXX      X XXXXXXXXXXX  XX    XXXXX    ',
+'        XXX                             ',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
